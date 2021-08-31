f4bc6ffaf92faec0e3e4a481665f6b3f29eed1a8
fedyksr <60010253+fedyksr@users.noreply.github.com>
2021-08-31 03:42:04 +0100

    msata

--- cuhc/models.py
+++ cuhc/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -10,12 +7,14 @@ class Etudiant(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200, null=True, blank=True)
 
+def __str__(self):
+        return self.full_name
+
 class article (models.Model):
     title=models.CharField(max_length=50)
     desc=models.TextField()
+    images=models.ImageField(upload_to='')
     created_at=models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.full_name
     def __str__(self):
         return self.title

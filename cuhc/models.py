acdc16ed606dcbd78a3c3e9cd6d85d17ccbafc60
fedyksr <60010253+fedyksr@users.noreply.github.com>
2021-08-31 01:09:03 +0100

    ضتخعف عى كخيثم ىخككثق ضقفهؤمث

--- cuhc/models.py
+++ cuhc/models.py
@@ -10,5 +10,12 @@ class Etudiant(models.Model):
     full_name = models.CharField(max_length=200)
     email = models.CharField(max_length=200, null=True, blank=True)
 
+class article (models.Model):
+    title=models.CharField(max_length=50)
+    desc=models.TextField()
+    image=models.ImageField(upload_to='')
+    created_at=models.DateTimeField(auto_now_add=True)
+    uploded_at=models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.full_name
